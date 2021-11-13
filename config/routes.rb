137c44d0377e568096869d2d39a4d2ae2b59e6f3
ryu-nuno <shion9bsk@gmail.com>
2021-11-13 05:46:16 +0000

    create top

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
 
-
-   root to: 'homes#top'
+  namespace :public do
+    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
