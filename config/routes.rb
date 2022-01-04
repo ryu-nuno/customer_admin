290f26aaa11dd67016543d7c6acc605e34ecf410
ryu-nuno <shion9bsk@gmail.com>
2022-01-04 23:47:42 +0000

    create public

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   namespace :public do
     root to: 'homes#top'
+    resources :customers, only: [:new, :create, :index, :show, :destroy, :edit , :update], to: 'public/customers#'
+    resources :histories, only: [:new, :create, :edit, :update, :destroy], to: 'public/histories#'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
